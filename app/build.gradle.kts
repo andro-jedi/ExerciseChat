b3ca29cade410610f51ff44eabbd50d13c8d36bd
Ihnatiev Serhii <17492722+thats-bot@users.noreply.github.com>
2024-08-10 20:05:19 +0200

    Add room with ksp dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.android.application")
+    kotlin("android")
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -63,4 +64,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    ksp(libs.room.compiler)
+    implementation(libs.room.runtime)
+    implementation(libs.room.ktx)
 }
